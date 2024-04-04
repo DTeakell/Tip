351b4ac693e92457a2b73b0626f12b2985867d32
Dillon Teakell <teakelld@icloud.com>
2024-04-04 13:29:57 -0400

    Initial Commit

---
+++ Tip/TipApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipApp.swift
+//  Tip
+//
+//  Created by Dillon Teakell on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
